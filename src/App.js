2f5c1fa73fec47e14811838aeb9add80de913472
shrutik <shrutik.mali15@gmail.com>
2024-05-13 13:13:41 +0530

    Create Conversion component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Upload from "./components/upload/Upload";
+import Conversion from "./components/conversion/Conversion";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       </div>
       <div className="body">
         <div className="app-upload"><Upload /></div>
-        <div className="app-convert"></div>
+        <div className="app-convert"><Conversion /></div>
       </div>
     </div>
   );
---
+++ src/components/conversion/Conversion.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./conversion.css";
+
+const Conversion = () => {
+  return (
+    <div className="conversion-root">
+      <p className="conversion-text">The text in image will appear here</p>
+    </div>
+  );
+}
+
+export default Conversion;
